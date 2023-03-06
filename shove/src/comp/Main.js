306817b38c74ba574d7acf1a899ec2cccfc3bdec
Minh Tran <tran0463@algonquinlive.com>
2023-03-06 02:36:41 -0500

    list rendering added

--- shove/src/comp/Main.js
+++ shove/src/comp/Main.js
@@ -1,37 +1,68 @@
+// export default function Main() {
+//     return (
+//         <div className="main">
+//             <h2>Tasks</h2>
+//             <div className="task">
+//                 <div>
+//                     <input type="checkbox" />
+//                     <h3>Task Name</h3>
+//                 </div>
+//             </div>
+//             <div className="task">
+//                 <div>
+//                     <input type="checkbox" />
+//                     <h3>Task Name</h3>
+//                 </div>
+//             </div>
+//             <div className="task">
+//                 <div>
+//                     <input type="checkbox" />
+//                     <h3>Task Name</h3>
+//                 </div>
+//             </div>
+//             <div className="task">
+//                 <div>
+//                     <input type="checkbox" />
+//                     <h3>Task Name</h3>
+//                 </div>
+//             </div>
+//             <div className="task">
+//                 <div>
+//                     <input type="checkbox" />
+//                     <h3>Task Name</h3>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+
+
+// list rendering
+
+const listItems = [
+    {task:"Task 1"},
+    {task:"Task 2"},
+    {task:"Task 3"},
+    {task:"Task 4"},
+    {task:"Task 5"}
+]
+
 export default function Main() {
+    const list = listItems.map(item => 
+        <div className="task">
+            <div>
+                <input type="checkbox" />
+                <ul>{item.task}</ul>
+            </div>
+        </div>
+    )
+
     return (
         <div className="main">
             <h2>Tasks</h2>
-            <div className="task">
-                <div>
-                    <input type="checkbox" />
-                    <h3>Task Name</h3>
-                </div>
-            </div>
-            <div className="task">
-                <div>
-                    <input type="checkbox" />
-                    <h3>Task Name</h3>
-                </div>
-            </div>
-            <div className="task">
-                <div>
-                    <input type="checkbox" />
-                    <h3>Task Name</h3>
-                </div>
-            </div>
-            <div className="task">
-                <div>
-                    <input type="checkbox" />
-                    <h3>Task Name</h3>
-                </div>
-            </div>
-            <div className="task">
-                <div>
-                    <input type="checkbox" />
-                    <h3>Task Name</h3>
-                </div>
-            </div>
+                {list}
         </div>
     )
 }
